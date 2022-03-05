b4687d41ffab2637c8556775c0c1cd6db0cfb3ab
Jesse Kovash <jessekovash@gmail.com>
2022-03-04 18:53:06 -0700

    all commands completed for seeding the database and manipulating the data. DB structured correctly and fully loaded.

--- Database/mongo.js
+++ Database/mongo.js
@@ -4,7 +4,7 @@ const config = require('./../config.js');
 
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri = 'mongodb://localhost/27017';
-const dbName = 'SDCtest';
+const dbName = 'SDC';
 const client = new MongoClient(uri);
 
 async function run() {
@@ -12,86 +12,79 @@ async function run() {
     await client.connect();
 
     const db = client.db(dbName);
-    const reviews = db.collection('reviews');
 
-    // query for movies that have a runtime less than 15 minutes
-    // const query = { runtime: { $lt: 15 } };
+    //---------code for merging characteristics and characteristic reviews (4 steps) -------
 
-    const options = {
-      // sort returned documents in ascending order by title (A->Z)
-      sort: { title: 1 },
-      // Include only the `title` and `imdb` fields in each returned document
-      projection: { _id: 0, title: 1, imdb: 1 },
-    };
+    //------(step 2) runfirst: adding name field to characteristic_reviews and transferring name value from characteristics-----
+    //(step 1) add new name field to characteristic_reviews_source via command line first
+    // const characteristic_reviews = db.collection('characteristic_reviews');
+    // const characteristics = db.collection('characteristics');
+    // const testing = await characteristic_reviews.aggregate([
+    //   {
+    //     $lookup: {
+    //       from: "characteristics",
+    //       localField: "characteristic_id",
+    //       foreignField: "id",
+    //       as: "name"
+    //     }
+    //   },
+    //   { $set: {
+    //     name: {$arrayElemAt: ["$name.name", 0]}
+    //   }
+    //   }, { $out: "merged_characteristics" }
+    // ]);
+    //-------end of adding name field and transferring name value
+    //end of code for merging characteristics and characteristic reviews
 
-    // const cursor = movies.find(query, options);
-    const cursor = reviews.find();
+    //--------(step 3) code for merging reviews with characteristic_reviews
+    const reviews = db.collection('reviews_source');
+    const merged_characteristics = db.collection('merged_characteristics');
+    const testing = await reviews.aggregate([
+      {
+        $lookup: {
+          from: "merged_characteristics",
+          localField: "id",
+          foreignField: "review_id",
+          as: "characteristic_reviews"
+        }
+      },
+      { $out: "reviews_characteristics_merged" }
+    ]);
 
-    // print a message if no documents were found
-    if ((await cursor.count()) === 0) {
-      console.log("No documents found!");
-    }
+    //--------end of code for merging reviews with characteristic_reviews
+    //(step 4)Run the scripts that modify the reviews table to match characteristics
 
-    // replace console.dir with your callback to access individual elements
-    await cursor.forEach((doc) => {
-      console.dir(doc);
+    //---------code for merging reviews and photos (step 5) ---------
+    // const reviews = db.collection('reviews_source');
+    // const photos = db.collection('reviews_photos');
+    // const testing = await reviews.aggregate([
+    //   {
+    //     $lookup: {
+    //       from: "reviews_photos",
+    //       localField: "id",
+    //       foreignField: "review_id",
+    //       as: "photos"
+    //     }
+    //   },
+    //   { $out: "merged_reviews" }
+    // ]);
+    //-----------end of code for merging reviews and photos ---------
+    await testing.forEach((doc) => {
+      if (doc.id == 5) {
+        console.dir(doc)
+      }
     });
+
   } finally {
     await client.close();
   }
 }
 run().catch(console.dir);
 
-
-// const mongoose = require('mongoose');
-// // mongoose.connect('mongodb://localhost/27017:SDCtest');
-// var mongoDB = 'mongodb://localhost/27017:SDCtest';
-// mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
-
-// //Get the default connection
-// var db = mongoose.connection;
-
-// //Bind connection to error event (to get notification of connection errors)
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// const prods = db.reviews.find({});
-// console.log(prods);
-
-// const { MongoClient } = require("mongodb");
-// // Connection URI
-// const uri =
-//   "mongodb+srv://localhost:27017/?maxPoolSize=20&w=majority";
-// // Create a new MongoClient
-// const client = new MongoClient(uri);
-// async function run() {
-//   try {
-//     // Connect the client to the server
-//     await client.connect();
-//     // Establish and verify connection
-//     await client.db("SDCtest").command({ ping: 1 });
-//     console.log("Connected successfully to server");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-
-// db.find({review_id: 1})
-
-// const dfd = require("danfojs-node")
-// dfd.readCSV("./mock_reviews.csv")
-// .then(df => {
-//   console.log('works')
-// })
-// .catch(err => {
-//   console.log(err)
-// })
-
-
+//--------command line inputs ---------
+//--------------------------------------
 //commandline to add files:
-// mongoimport --db=SDC --collection=reviews --type=csv --headerline --file=Data/reviews.csv
+// mongoimport --db=SDCtest --collection=reviews --type=csv --headerline --file=Data/mock_reviews.csv
 // mongoimport --db=SDC --collection=reviews_photos --type=csv --headerline --file=Data/reviews_photos.csv
 // mongoimport --db=SDC --collection=characteristics --type=csv --headerline --file=Data/characteristics.csv
 // mongoimport --db=SDC --collection=characteristic_reviews --type=csv --headerline --file=Data/characteristic_reviews.csv
@@ -99,7 +92,7 @@ run().catch(console.dir);
 //type 'mongo' in the command line to switch to mongodb shell. allows access to the db/collections
 
 //creating indexes on a duplicate key:
-//db.reviews.createIndex({ "product_id": 1 }, {"unique": false})
+//db.reviews_characteristics_merged.createIndex({ "product_id": 1 }, {"unique": false})
 
 //searching an indexed key and verifying it only searched the relevant files
 //db.reviews.find( {"product_id":12}).explain("executionStats")
@@ -109,6 +102,23 @@ run().catch(console.dir);
 
 //gets all ids without duplicates
 //db.reviews.distinct("product_id")
+
+//rename a collection
+// db.reviews.renameCollection("reviews_source")
+
+//add a new field to all documents in a collection
+// db.characteristic_reviews.update({},{$set:{"name":""}}, false, true)
+
+//delete a field from all documents in a collection
+//run all of these to correctly format the reviews/meta merged document (step 4 from)
+// db.reviews_characteristics_merged.update({}, {$unset: {date:1}}, false, true);
+// db.reviews_characteristics_merged.update({}, {$unset: {summary:1}}, false, true);
+// db.reviews_characteristics_merged.update({}, {$unset: {body:1}}, false, true);
+// db.reviews_characteristics_merged.update({}, {$unset: {reviewer_name:1}}, false, true);
+// db.reviews_characteristics_merged.update({}, {$unset: {reviewer_email:1}}, false, true);
+// db.reviews_characteristics_merged.update({}, {$unset: {response:1}}, false, true);
+// db.reviews_characteristics_merged.update({}, {$unset: {helpfulness:1}}, false, true);
+
 
 
 //**************** */
